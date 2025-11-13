860d210aced30355b69a5469bae9c9425e22bcb8
Quan12345689 <truonghungquan119@gmail.com>
2025-11-13 18:29:00 +0700

    Slight adjust on Footer and Header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@
 import { useState } from 'react';
 import { AppBar, Toolbar, Button, TextField, Box, Typography, InputAdornment, IconButton, Grid, Stack } from '@mui/material';
 import { FooterImages } from '@/const';
-import { Send, Twitter, YouTube, Instagram, Facebook } from '@mui/icons-material';
+import { Twitter, YouTube, Instagram, Facebook } from '@mui/icons-material';
+import { CommonIcons } from './Icons';
+import '@/themes/styles/global.scss';
 
 const COMPANY_ITEMS = ['About us', 'Blog', 'Contact us', 'Pricing', 'Testimonials'];
 const SUPPORT_ITEMS = ['Help Center', 'Term of Services', 'Legal', 'Privacy Policy', 'Status'];
@@ -17,63 +19,70 @@ export function Footer() {
   return (
     <AppBar position="static" sx={{ backgroundColor: 'secondary.main', color: '#fff' }}>
       <Toolbar
-        disableGutters
         sx={{
-          px: { xs: 2, sm: 4, md: 6 },
-          py: { xs: 4, sm: 6 },
+          py: { xs: 4, md: 8 }, px: { xs: 4, md: 20 },
           display: 'flex',
-          justifyContent: 'center'
+          alignItems: 'center',
         }}
       >
-        <Grid container columnSpacing={{ xs: 2, sm: 4, md: 5, lg: 12 }} rowSpacing={{ xs: 3, sm: 4, md: 5 }}>
+        <Grid container
+          spacing={{ xs: 4, sm: 32, md: 48 }}
+          alignItems="flex-start"
+          justifyContent="space-between">
 
           {/* Logo & Social */}
           <Grid item xs={12} md={3}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: { xs: 1, sm: 2, md: 3 } }}>
-              <img src={FooterImages.footlogo} alt="Company Logo" style={{ width: 180, height: 'auto' }} />
-              <Typography variant="body3_r">
+            <Stack spacing={5}>
+              <img src={FooterImages.footlogo} alt="Company Logo" />
+              <Typography variant="body3_r" lineHeight={1.6}>
                 Copyright © 2020 Nexcent ltd. <br /> All rights reserved
               </Typography>
               <Box sx={{ display: 'flex', gap: { xs: 1, sm: 2 } }}>
-                <IconButton color="primary"><Instagram fontSize="medium" /></IconButton>
-                <IconButton color="primary"><Facebook fontSize="medium" /></IconButton>
-                <IconButton color="primary"><Twitter fontSize="medium" /></IconButton>
-                <IconButton color="primary"><YouTube fontSize="medium" /></IconButton>
+                <IconButton className='social-icon'><Instagram fontSize="medium" /></IconButton>
+                <IconButton className='social-icon'><Facebook fontSize="medium" /></IconButton>
+                <IconButton className='social-icon'><Twitter fontSize="medium" /></IconButton>
+                <IconButton className='social-icon'><YouTube fontSize="medium" /></IconButton>
               </Box>
-            </Box>
+            </Stack>
           </Grid>
 
           {/* Company Links */}
           <Grid item xs={10} sm={6} md={2}>
-            <Stack spacing={1}>
-              <Typography variant="h4">Company</Typography>
-              {COMPANY_ITEMS.map((label, idx) => (
-                <Button
-                  key={idx}
-                  variant="text"
-                  sx={{ color: '#fff', justifyContent: 'flex-start', padding: 0, minWidth: 0 }}
-                >
-                  <Typography variant="body3_r">{label}</Typography>
-                </Button>
-              ))}
-            </Stack>
-          </Grid>
+            <Grid container spacing={12}>
+              <Grid item xs={6} sm={6}
+              sx ={{ display: 'flex', flexDirection: 'column' , justifyContent: 'flex-start' }}>
+                <Typography variant="h4" sx={{ paddingBottom: '20px' }}>Company</Typography>
+                <Stack>
+                  {COMPANY_ITEMS.map((label, idx) => (
+                    <Button
+                      key={idx}
+                      variant="text"
+                      className="footer-link"
+                    >
+                      <Typography variant="body3_r">{label}</Typography>
+                    </Button>
+                  ))}
+                </Stack>
+              </Grid>
 
-          <Grid item xs={10} sm={6} md={2}>
-            <Stack spacing={1}>
-              <Typography variant="h4">Support</Typography>
-              {SUPPORT_ITEMS.map((label, idx) => (
-                <Button
-                  key={idx}
-                  variant="text"
-                  sx={{ color: '#fff', justifyContent: 'flex-start', padding: 0, minWidth: 0 }}
-                >
-                  <Typography variant="body3_r">{label}</Typography>
-                </Button>
-              ))}
-            </Stack>
-          </Grid>
+              {/* Support Links */}
+              <Grid item xs={6} sm={6}
+              sx ={{ display: 'flex', flexDirection: 'column' , justifyContent: 'flex-start' }}>
+                <Typography variant="h4" sx={{ paddingBottom: '20px' }}>Support</Typography>
+                <Stack>
+                  {SUPPORT_ITEMS.map((label, idx) => (
+                    <Button
+                      key={idx}
+                      variant="text"
+                      className="footer-link"
+                    >
+                      <Typography variant="body3_r">{label}</Typography>
+                    </Button>
+                  ))}
+                </Stack>
+              </Grid>
 
+          
           {/* Stay Up to Date */}
           <Grid item xs={12} sm={6} md={3}>
             <Stack direction="column" spacing={2}>
@@ -81,20 +90,26 @@ export function Footer() {
               <TextField
                 fullWidth
                 label="Your email address"
-                color="primary"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 InputProps={{
+                  className: 'email-type-field',
                   endAdornment: (
                     <InputAdornment position="end">
-                      <IconButton color="secondary" onClick={handleSend}>
-                        <Send />
+                      <IconButton onClick={handleSend}>
+                        <CommonIcons.emailicon />
                       </IconButton>
                     </InputAdornment>
                   ),
                 }}
+                InputLabelProps={{
+                  className: 'email-type-label'
+                }}
               />
             </Stack>
+          </Grid>
+          
+            </Grid>
           </Grid>
         </Grid>
       </Toolbar>
