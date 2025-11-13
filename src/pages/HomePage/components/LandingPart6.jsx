3320166d223e546d99fbe54b68acf1e032edc6f1
Quan12345689 <truonghungquan119@gmail.com>
2025-11-13 10:34:02 +0700

    small change to shorten the code

--- src/pages/HomePage/components/LandingPart6.jsx
+++ src/pages/HomePage/components/LandingPart6.jsx
@@ -33,14 +33,7 @@ export function LandingPart6() {
                         British Dragon Boat Racing Association
                     </Typography>
 
-                    <Stack
-                        direction="row"
-                        flexWrap="wrap"
-                        justifyContent="center"
-                        alignItems="center"
-                        mt={4}
-                        gap={3}
-                    >
+                    <Stack className= "Stack-container" >
                         {clients.map((client, idx) => (
                             <Box key={idx} sx={{ display: 'flex', justifyContent: 'center' }}>
                                 <img src={client} alt={`Client${idx + 1}`} />
