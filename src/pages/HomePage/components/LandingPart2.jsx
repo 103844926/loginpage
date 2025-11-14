06bd72ee992cbe269b855f86ce37d38b187b998c
Quan12345689 <truonghungquan119@gmail.com>
2025-11-14 11:21:59 +0700

    removing global.scss '!important', adjust page layout

--- src/pages/HomePage/components/LandingPart2.jsx
+++ src/pages/HomePage/components/LandingPart2.jsx
@@ -1,7 +1,8 @@
 import { Box, Button, Typography, Container, Grid, Stack } from '@mui/material'
 import { HomeImages } from '@/const';
-import { Landing2 } from '@/components/Icons';
-import '@/themes/styles/global.scss'
+import { Landing2 } from '@/components/icons';
+import { gap } from '@mui/system';
+
 
 const clients = [
     HomeImages.landlogo1,
@@ -14,14 +15,20 @@ const clients = [
 ];
 
 export function LandingPart2() {
-    const shadowbox ={
+    const shadowBox = {
         boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.08)'
     };
 
+    const landingIcon = {
+        width: 65,
+        height: 56,
+        '& path:first-of-type': { color: 'color_tint.t5' },
+        '& path:last-of-type': { color: 'color_shade.s5' },
+    };
     return (
         <>
             <Box sx={{ backgroundColor: 'neutral.white', py: { xs: 4, md: 8 }, px: { xs: 4, md: 20 } }}>
-                <Container maxWidth="lg">
+                <Container maxWidth="lg" className="column-center">
                     <Grid
                         container
                         spacing={1}
@@ -30,16 +37,16 @@ export function LandingPart2() {
                         <Typography variant="h2" color="neutral.grey_dark">
                             Our Clients
                         </Typography>
-                        <Typography variant="body2_r" color="neutral.grey_dark">
+                        <Typography variant="body2_r" color="neutral.grey">
                             We have been working with some Fortune 500+ clients
                         </Typography>
                     </Grid>
 
-                    <Stack className="stack-container">
+                    <Stack className= "stack-row-container" gap={10} >
                         {clients.map((client, idx) => (
-                            <Box key={idx} sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Grid item xs={6} sm={4} md={2} key={idx} textAlign="center">
                                 <img src={client} alt={`Client${idx + 1}`} />
-                            </Box>
+                            </Grid>
                         ))}
                     </Stack>
                 </Container>
@@ -51,15 +58,15 @@ export function LandingPart2() {
                         <Typography variant="h2" color="neutral.grey_dark" >
                             Manage your entire community <br /> in a single system
                         </Typography>
-                        <Typography variant="body2_r" color="neutral.grey_dark" >
+                        <Typography variant="body2_r" color="neutral.grey" >
                             Who is Nexcent suitable for?
                         </Typography>
                     </Grid>
 
-                    <Grid container className="row-center" >
+                    <Grid container className="row-center" sx={{ gap: '100px' }} >
                         <Grid item xs={12} sm={6} md={4}>
-                            <Box className="small-column-center" sx={shadowbox}>
-                                    <Landing2.LandingMember />
+                            <Box className="small-column-center" sx={shadowBox}>
+                                <Landing2.LandingMember sx={landingIcon} />
                                 <Typography variant="h3" color="neutral.grey_dark" >
                                     Membership Organizations
                                 </Typography>
@@ -70,8 +77,8 @@ export function LandingPart2() {
                         </Grid>
 
                         <Grid item xs={12} sm={6} md={4}>
-                            <Box className="small-column-center" sx={shadowbox}>
-                                <Landing2.LandingAssociation />
+                            <Box className="small-column-center" sx={shadowBox}>
+                                <Landing2.LandingAssociation sx={landingIcon} />
                                 <Typography variant="h3" color="neutral.grey_dark" >
                                     National Associations
                                 </Typography>
@@ -82,8 +89,8 @@ export function LandingPart2() {
                         </Grid>
 
                         <Grid item xs={12} sm={6} md={4}>
-                            <Box className="small-column-center" sx={shadowbox}>
-                                <Landing2.LandingClubGroup />
+                            <Box className="small-column-center" sx={shadowBox}>
+                                <Landing2.LandingClubGroup sx={landingIcon} />
                                 <Typography variant="h3" color="neutral.grey_dark" >
                                     Clubs And Groups
                                 </Typography>
